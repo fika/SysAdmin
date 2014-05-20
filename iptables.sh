17d5930eb4f230b7132985d3eeb48d83efb11c1e
Love Dufva <love@lovedufva.se>
2014-05-20 10:41:57 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -39,5 +39,89 @@ fi
 
 
 # ~~~~~~~~~~  BEGINNING OF SCRIPT BELOW ~~~~~~~~~ #
-echo -e "${BLUE_TEXT}TEST TEST TEST TEST${END}"
+#ENTER MENU
+clear
+echo ""
+echo ""
+echo -e "${RED_TEXT}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${END}"
+echo -e "${RED_TEXT}~~~~~~~~~~~~~~~~~~~~~~~~             IPTABLES            ~~~~~~~~~~~~~~~~~~~~~~~~~${END}"
+echo -e "${RED_TEXT}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${END}"
+echo ""
+echo ""
+echo -e "${BLUE_TEXT}1) List existing iptables configuration.${END}"
+echo ""
+echo -e "${BLUE_TEXT}2) Start, stop or restart firewall.${END}"
+echo ""
+echo -e "${BLUE_TEXT}3) Sample text here.${END}"
+echo ""
+echo -e "${BLUE_TEXT}4) Sample text here.${END}"
+echo ""
+echo -e "${BLUE_TEXT}5) Sample text here.${END}"
+echo ""
+echo -e "${BLUE_TEXT}6) Sample text here.${END}"
+echo ""
+echo -e "${BLUE_TEXT}7) Sample text here.${END}"
+echo ""
+echo -e "${BLUE_TEXT}8) Kill all open tunnels.${END}"
+echo ""
+echo -e "${BLUE_TEXT}9) Quit the program${END}"
 
+# Look for options
+echo -e ""
+echo -e "${WARP}SELECT:${END}"
+read option
+
+
+
+if [ "$option" = "1" ]; then
+	#SHOW IPTABLES RULES
+	iptables -L -n -v
+
+elif [ "$option" = "2" ]; then
+	#STARTING sample
+	echo -e "${BLUE_TEXT}What do you want to do?{END}"
+	echo -e "${BLUE_TEXT}1) Start firewall.{END}"
+	echo -e "${BLUE_TEXT}2) Stop firewall.{END}"
+	echo -e "${BLUE_TEXT}3) Restart firewall{END}"
+	read twoption
+		if [ "$twoption" = "1" ]; then
+			service iptables start
+		elif [ "$twoption" = "2" ]; then
+			service iptables stop
+		elif [ "$twoption" = "3" ]; then
+			service iptables restart
+		else
+			cowsay 'TRY AGAIN, FOOL!'
+		fi
+
+elif [ "$option" = "3" ]; then
+	#STARTING sample
+	bash sample
+
+elif [ "$option" = "4" ]; then
+	#STARTING sample
+	bash sample
+
+elif [ "$option" = "5" ]; then
+	#STARTING sample
+	bash sample
+
+elif [ "$option" = "6" ]; then
+	#STARTING sample
+	python sample
+
+elif [ "$option" = "7" ]; then
+	#STARTING sample
+	bash sample
+
+elif [ "$option" = "8" ]; then
+	#KILL ALL
+	killall ssh
+
+elif [ "$option" = "9" ]; then
+	echo "Exiting"
+	exit 0
+else
+echo "Please try again."
+bash SysAdmin
+fi
