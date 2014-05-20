999306bd6d56c15e857bcec4a591c6e813c1947d
volten <viktor.voltaire@gmail.com>
2014-05-20 17:21:08 +0200

    Update reversetunnel.sh

--- reversetunnel.sh
+++ reversetunnel.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-#Author Volten
+#Author Viktor "Volten" Voltaire
 echo -e "Enter the host you want to tunnel through"
 read hostip
-echo -e "\nEnter the port you want to forward with (example 2210)"
+echo -e "Enter the port you want to forward with (example 2210)"
 read forport
-echo -e "\nEnter localhost or the ip of another host, like a printer"
+echo -e "Enter localhost or the ip of another host, like a printer"
 read localip
-echo -e "\nEnter the port of the service you will tunnel (example 22 for ssh)"
+echo -e "Enter the port of the service you will tunnel (example 22 for ssh)"
 read localport
-echo -e "\nEnter the username for the gateway"
+echo -e "Enter the username for the gateway"
 read user
 
 ssh -N -f -R $hostip:$forport:$localip:$localport $user@$hostip
-echo "\nMake sure that the gateway has GatewayPorts clientspecified in sshd_config\n"
+echo ""
+echo "Make sure that the gateway has GatewayPorts clientspecified in sshd_config"
+echo ""
 read -r -p "Do you want to terminate the ssh tunnel? [y/N] " response
 case $response in
 [yY][eE][sS]|[yY])
        killall ssh
 ;;
     *)
-        echo "\nGG"
+        echo ""
+        echo "GG"
 ;;
 esac
