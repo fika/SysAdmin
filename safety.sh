0d38b843c887c486e19b012af28671f27ed9edc2
volten <viktor.voltaire@gmail.com>
2014-05-20 13:17:50 +0200

    Update safety.sh

--- safety.sh
+++ safety.sh
@@ -1,55 +1,36 @@
 #!/bin/bash
-#####################################################################################################################
-# #
-# This script is written by Johan aka Saint, admin of Webbh4tt #
-# This program is open source; you can redistribute it and/or modify it under the terms of the GNU General Public #
-# The author bears no responsibility for malicious or illegal use. #
-# #
-# #
-#####################################################################################################################
 
-#########################################################################################
-#                                   known bugs                                          #
-#                                   None so far                                         #
-#                     Please report bugs to info@webbhatt.com                           #
-#########################################################################################
+#exempel på en standardFW
 
-# ~~~~~~~~~~ Environment Setup ~~~~~~~~~~ #
-. ./environment
-# ~~~~~~~~~~ Environment Setup ~~~~~~~~~~ #
-#info
-#Author Johan "Saint" B�rjesson
 
-#################################Start script and validation####################################
-#
-# Till en börja kommer detta se ut som majjens script. skriver detta som en grund för att kunna vidare modifiera enligt egna preferenser.
-# 
-cd /usr/local/sbin
+iptables -F
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables -A INPUT -p tcp -m tcp ! --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,SYN,RST,PSH,ACK,URG -j DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 2210 -j ACCEPT
+iptables -A INPUT -s 10.10.4.195/32 -p tcp -m tcp --dport 22 -j ACCEPT
 
-lista() {
-  echo 'ipv6'
-# ip6tables -L
-  ip6tables-save
-  echo 'ipv4'
-# iptables -L
-  iptables-save
-}
+
+
+
+
+
+#!/bin/bash
+
+#exempel, här kan man ha en entry read grejj
+iptables -F
 
 resetta() {
-  iptables -P INPUT ACCEPT
-  iptables -P FORWARD ACCEPT
-  iptables -P OUTPUT ACCEPT
-  iptables -F
-  iptables -X
-  ip6tables -F
-echo "Iptables är nu fixat"
-lista
+bash /home/student/standardfw.sh
+echo 'regler borta'
 }
-for (( i=30; i>0; i--)); do
-    printf "\rStarting script in $i seconds.  Hit any key to continue."
-    read -s -n 1 -t 1 key
-    if [ $? -eq 0 ]
-    then
-        break
-    fi
-done
+
+touch /var/run/FulWall.sh
+
+( echo '60 sekunder kvar'; sleep 30  ; echo '30 sekunder kvar'; sleep 30 ; rm -f /var/run/FulWall && resetta ) &
+
+#om du inte är utslängd	kan du nu bestämma att ta bort skiten å	spara till din standarfw
+
+exit 0
