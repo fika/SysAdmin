305bc2000b7dfa5fb6f13b98f92fc9d4585ace92
volten <viktor.voltaire@gmail.com>
2014-05-20 16:29:56 +0200

    Update safety.sh

--- safety.sh
+++ safety.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 # Author Volten, Saint
-. ./environment
-# Buggar som är kända, 
+#. ./environment
+# Buggar som är kända,
 # När man har blivit utsparkad vill man bara se att reglerna är borta inte valet om att ta bort Fulwall
 echo -e "Please enter Iptables rule:"
 read iprule
 $iprule
+
 resetta() {
 iptables-restore restora.fil
 }
 
-touch /var/run/FulWall.sh
+touch /var/run/FulWall
 
-( sleep 30 ; rm -f /var/run/FulWall && resetta ) &
-#
-# if true
-# ls /var/run/FulWall
-# do
-# resetta
+( sleep 10 ; rm -f /var/run/FulWall && resetta ) &
+
 read -r -p "Would you like to delete the FulWall file and save your changes to the default setup of iptables? [y/N] " response
 case $response in
 [yY][eE][sS]|[yY])
-rm -f /var/run/FulWall.sh
+rm -f /var/run/FulWall
 cp restora.fil restora.back
-iptables-save > restora.fil 
+iptables-save > restora.fil
+echo -e "${SUCCESS}[*] Rule have been added to default setup of iptables and a backup has been created of the old ones.${END}"
 ;;
     *)
-:
+echo ""
+echo "Your rule was not saved"
 ;;
 esac
-echo -e "${SUCCESS}[*] Rule have been added to default setup of iptables ${END}"
 exit 0
