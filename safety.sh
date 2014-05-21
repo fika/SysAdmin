d7652d0ce042218194eac6df85b8bce3887cd1de
volten <viktor.voltaire@gmail.com>
2014-05-21 11:43:41 +0200

    Update safety.sh

--- safety.sh
+++ safety.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Author Volten, Saint
 
+folder="/var/bak"
 iptables-save > temp.fil
 
 echo -e "Please enter Iptables rule:"
@@ -15,12 +16,18 @@ iptables-restore temp.fil
 
 echo -e "Skriv yes för att spara annars avbryts det om 10" 
 read -t 10 answer  
+
 if [ "$answer"  == "yes" ] ; then
-iptables-save > $outfile
+
+mv $folder/iptables.* $folder/old/
+iptables-save > $folder/$outfile
 rm temp.fil
 echo -e "Rule has been added"
+
 else
+
 resetta
 echo -e "Rule has not been added due to timeout"
 rm temp.fil
+
 fi
