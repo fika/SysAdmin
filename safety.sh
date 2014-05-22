3105d241eb6287e8ff40ad9dd41f8129bb50cb3b
volten <viktor.voltaire@gmail.com>
2014-05-22 17:00:47 +0200

    Update safety.sh

--- safety.sh
+++ safety.sh
@@ -2,34 +2,64 @@
 # Author Volten, Saint
 # LOVE HJÄLPTE OCKSÅ TILL
 
+#Variablar
+firsttime="0"
+temp="temp"
+now=$(date +"%d-%m-%y_%H%M_%S")
+outfile="iptables.$now"
+#
+
 #Måste köras som root
 if [[ $EUID -ne 0 ]]; then
 echo -e "${WARNING}Script must be run as root!${END}"
 exit 1
 else
 
+if [ "$firsttime" == "0" ] ;
+then
+echo -e "What folder do you want to use? (example /var/bak)"
+read instfolder
+mkdir -p $instfolder
+echo -e "What folder do you want to move the old iptables files? (example /var/bak/old)"
+read instold
+mkdir -p $instold
+
+sed -i '0,/firsttime="0"/s//firsttime="1"/' saftey.sh
+
+sed -i "7i\
+folder=$instfolder" saftey.sh
+
+sed -i "8i\
+old=$instold" saftey.sh
+
+read -r -p "Do you want to save your current iptables? [y/N] " response
+case $response in
+[yY][eE][sS]|[yY])
+        echo -e "\nInstall complete, reloading the script"
+        iptables-save > $instfolder/$outfile
+        bash saftey.sh
+;;
+    *)
+        echo -e "\nInstall complete, reloading the script"
+        bash saftey.sh
+;;
+esac
+
+else
+
+
 #Funktionen
 restore() {
 iptables-restore $temp
 }
-##
-#Variablar
-folder="/var/bak"
-mkdir -p $folder
-old="/var/bak/old"
-mkdir -p $old
-temp="temp"
-now=$(date +"%d-%m-%y_%H%M_%S")
-outfile="iptables.$now"
-##
-#Scriptet
+
 iptables-save > $temp
 
 echo -e "Enter Iptables rule:"
 read iprule
 $iprule
 
-read -t 20 -r -p "Do you want to save the rule? [y/N]" response
+read -t 20 -r -p "Do you want to save the rule? [y/N] " response
 case $response in
 [yY][eE][sS]|[yY])
         rm $temp
@@ -44,4 +74,5 @@ case $response in
 ;;
 esac
 
+fi
 fi
